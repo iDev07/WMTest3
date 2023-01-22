efcf1dbe6789d502844e46e7aba3163a1a5f8aed
Diyorbek <idevdiyor@gmail.com>
2023-01-22 13:44:35 +0500

    Last chamges

--- wmtest3/components/MainLoader.js
+++ wmtest3/components/MainLoader.js
@@ -1,18 +1,24 @@
 import React from "react";
 import { FallingLines } from "react-loader-spinner";
+import Head from "next/head";
 
 function MainLoader() {
   return (
-    <div align="center" className="loader_main">
-      <FallingLines
-        type="Plane"
-        style={{ display: "flex", backgroundColor: "#000" }}
-        color="#fff"
-        height={100}
-        width={100}
-        timeout={20000}
-      />
-    </div>
+    <>
+      <Head>
+        <title>World Miral</title>
+      </Head>
+      <div align="center" className="loader_main">
+        <FallingLines
+          type="Plane"
+          style={{ display: "flex", backgroundColor: "#000" }}
+          color="#fff"
+          height={100}
+          width={100}
+          timeout={20000}
+        />
+      </div>
+    </>
   );
 }
 
