613504ba2532fdd9492192d481c412cec03c028e
Diyorbek <idevdiyor@gmail.com>
2023-07-28 14:45:18 +0500

    Script changed

--- wmtest3/pages/_document.js
+++ wmtest3/pages/_document.js
@@ -45,15 +45,15 @@ export default function Document() {
         {/* Global Site Tag (gtag.js) - Google Analytics */}
         <script
           async
-          src="https://www.googletagmanager.com/gtag/js?id=G-PV0GY6KYJD"
-        ></script>
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
         <Script id="google-analytics">
           {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
  
-          gtag('config', 'G-PV0GY6KYJD');
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
         `}
         </Script>
       </Head>
